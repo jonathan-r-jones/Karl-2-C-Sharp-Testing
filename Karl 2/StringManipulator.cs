1213ebf9b3059614ee188964f53604cd9fcf2fa8
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-02-16 14:21:43 -0500

    Feb-16-2018_2:21_PM

--- Karl 2/StringManipulator.cs
+++ Karl 2/StringManipulator.cs
@@ -8,9 +8,9 @@ namespace Karl_2
         {
             return Name + "_One";
         }
-        public string ReverseString(string guineaString)
+        public string ReverseString(string sutString)
         {
-            char[] inputarray = guineaString.ToCharArray();
+            char[] inputarray = sutString.ToCharArray();
             Array.Reverse(inputarray);
             string output = new string(inputarray);
             return output;
